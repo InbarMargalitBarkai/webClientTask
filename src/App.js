93612e5916bd3eabce8e9638db86d2c0950d35a3
InbarMargalitBarkai <inbarbarkai6@gmail.com>
2022-04-13 23:52:49 +0300

    fix: adding html of register and login screen also try to start work on chat board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+////import logo from './logo.svg';
+////import './App.css';
 
-function App() {
-//     return (
-//         <form method="post">
-//             <label>Username</label>
-//             <input type="text" required></input>
-//                 <p> {/*<!---add space between the two lines--->*/}
-//                 <label>Password</label>&nbsp;
-//                 </p>
-//                 <input type="password" required></input>
-//                         <p>
-//                             <button type="button" onclick="console.log('e')">Login</button>
-//                             <div>
-//                                 <label>Not registered?</label>
-//                                 <a href=" ">Click here</a>
-//                                 <label>to register</label>
-//                             </div>
-//                         </p>
-//         </form>
-//   );
-}
+////function App() {
+//////     return (
+//////         <form method="post">
+//////             <label>Username</label>
+//////             <input type="text" required></input>
+//////                 <p> {/*<!---add space between the two lines--->*/}
+//////                 <label>Password</label>&nbsp;
+//////                 </p>
+//////                 <input type="password" required></input>
+//////                         <p>
+//////                             <button type="button" onclick="console.log('e')">Login</button>
+//////                             <div>
+//////                                 <label>Not registered?</label>
+//////                                 <a href=" ">Click here</a>
+//////                                 <label>to register</label>
+//////                             </div>
+//////                         </p>
+//////         </form>
+//////   );
+////}
 
-export default App;
+////export default App;
