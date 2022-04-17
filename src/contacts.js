a76c210bd0d49721087c6da37f6c5282aef6849d
Moria Yefet <moria.yefet@gmail.com>
2022-04-17 16:36:21 +0300

    fix: finish the register screen (without styling).

---
+++ src/contacts.js
@@ -0,0 +1,32 @@
+const contactsList = [
+    {
+        Username: 'Lisa',
+    Nickname: 'Lisa',
+    Password: '1l1234567',
+    photoUrl: 'https://talkjs.com/images/avatar-1.jpg'
+            },
+    {
+        Username: 'Alice',
+    Nickname: 'Alice',
+    Password: '2a1234567',
+    photoUrl: 'https://talkjs.com/images/avatar-2.jpg'
+            },
+    {
+        Username: 'Tina',
+    Nickname: 'Tina',
+    Password: '3t1234567',
+    photoUrl: 'https://talkjs.com/images/avatar-3.jpg'
+            },
+    {
+        Username: 'Lee',
+    Nickname: 'Lee',
+    Password: '4l1234567',
+    photoUrl: 'https://talkjs.com/images/avatar-4.jpg'
+            },
+    {
+        Username: 'Pete',
+    Nickname: 'pete',
+    Password: '5p1234567',
+    photoUrl: 'https://talkjs.com/images/avatar-5.jpg'
+            },
+    ];
